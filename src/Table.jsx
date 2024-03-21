5e9b40e3747f19d3f3f57e95e414193a3f8144e1
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-03-21 12:53:11 +0530

    modified-code

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import "./table.css";
 import { userData } from "./constant";
@@ -7,12 +6,12 @@ function Table() {
   const [type, setType] = useState("");
   const [gender, setGender] = useState("");
   const [table, setTable] = useState(userData);
+  const [lastseen, setLastseen] = useState("");
 
   const handle = () => {
     if (type?.length > 0) {
-
       const filterTab = userData.filter((item) =>
-        item.name.toLowerCase().includes(type)
+        item.name.toLowerCase().includes(type.toLowerCase())
       );
       setTable(filterTab);
     } else {
@@ -20,73 +19,93 @@ function Table() {
     }
   };
 
-  // const changeGender = () => {
-  //   if (gender !== "All") {
-  //     const filterData = userData.filter((item) => item.gender === gender);
-  //     setTable(filterData);
-  //   } else {
-  //     setTable(userData);
-  //   }
-  // };
-  function changeGender(e) {
-    setGender(e.target.value);
+  const changeGender = () => {
     if (gender !== "All") {
-      const filterData = userData.filter(
-        (item) => item.gender === e.target.value
-      );
+      const filterData = userData.filter((item) => item.gender === gender);
       setTable(filterData);
     } else {
       setTable(userData);
     }
-  }
+  };
+
+  const last = () => {
+    if (lastseen !== "All-years") {
+      const filter = userData.filter((item) =>
+        item.lastSeen.endsWith(lastseen)
+      );
+      setTable(filter);
+    } else {
+      setTable(userData);
+    }
+  };
+
+  useEffect(() => {
+    setTable(table);
+    // Table();
+  }, []);
 
   useEffect(() => {
     handle();
   }, [type]);
 
-  // useEffect(() => {
-  //   changeGender();
-  // }, [gender]);
+  useEffect(() => {
+    changeGender();
+  }, [gender]);
+
+  useEffect(() => {
+    last();
+  }, [lastseen]);
 
   return (
     <div className="main">
       <div className="box">
-        <input
-          id="search"
-          type="text"
-          value={type}
-          placeholder="Search"
-          onChange={(e) => {
-            setType(e?.target?.value);
-          }}
-        />{" "}
-        {type}
-        <label htmlFor="gender">Gender</label>
-        <select
-          name="gender"
-          id="gender"
-          value={gender}
-          //   onChange={(e) => {
-          //     setGender(e?.target?.value);
-          //   }}
-          onChange={changeGender}
-        >
-          <option value="">select gender</option>
-          <option value="All">All</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
-        <label htmlFor="active">Active last</label>
-        <select name="active" id="active">
-          <option value="">select year</option>
-          <option value="All years">All years</option>
-          <option value="2005">2005</option>
-          <option value="2011">2011</option>
-          <option value="2015">2015</option>
-          <option value="2016">2016</option>
-          <option value="2017">2017</option>
-          <option value="2018">2018</option>
-        </select>
+        <div className="search">
+          <input
+            id="search"
+            type="text"
+            value={type}
+            placeholder="Search"
+            onChange={(e) => {
+              setType(e.target.value);
+            }}
+          />
+        </div>
+        <div className="gender">
+          <label htmlFor="gender">Gender</label>
+          <select
+            name="gender"
+            id="gender"
+            value={gender}
+            onChange={(e) => {
+              setGender(e.target.value);
+            }}
+          >
+            <option value="">select gender</option>
+            <option value="All">All</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <div className="last-seen">
+          <label htmlFor="active">Active last</label>
+          <select
+            name="active"
+            id="active"
+            value={lastseen}
+            onChange={(e) => {
+              setLastseen(e.target.value);
+            }}
+          >
+            <option value="">select year</option>
+            <option value="All-years">All years</option>
+            <option value="2005">2005</option>
+            <option value="2011">2011</option>
+            <option value="2015">2015</option>
+            <option value="2016">2016</option>
+            <option value="2017">2017</option>
+            <option value="2018">2018</option>
+          </select>
+        </div>
       </div>
       <table>
         <thead>
@@ -103,7 +122,7 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {table?.map((row, index) => (
+          {table.map((row, index) => (
             <tr key={index}>
               <td>{row.id}</td>
               <td>{row.name}</td>
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import Table from "./Table";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- 
-    <Table />
- 
+  <Table />
+  // Table()
 );
